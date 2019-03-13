3a0571c0d13b7c7fe6cbd1af770eb8ffc66eea9e
Omar Alhariri <omar.alhariri11@gmail.com>
2019-03-13 18:16:45 +0200

    DRY

--- routes.php
+++ routes.php
@@ -6,5 +6,5 @@ $router->define([
     "home" => "controllers/index.php",
     "about" => "controllers/about.php",
     "about/culture" => "controllers/about-culture.php",
-    "contact-our-company" => "controllers/contact.php"
+    "contact" => "controllers/contact.php"
 ]);
--- views/index.view.php
+++ views/index.view.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php require("partials/head.php"); ?>
+
+<h1>My Tasks</h1>
 <ul>
     <?php foreach ($tasks as $task) : ?>
         <li>
@@ -19,5 +13,4 @@
     <?php endforeach; ?>
 </ul>
 
-</body>
-</html>
+<?php require("partials/footer.php"); ?>
